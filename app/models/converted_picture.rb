6ab907d963b8dda72f6454b219f47f1204d16f76
Dragon-taro <o_o_o_o_o1031@yahoo.co.jp>
2018-05-27 16:01:25 +0900

    初期化

--- app/models/converted_picture.rb
+++
@@ -1,4 +0,0 @@
-class ConvertedPicture < ApplicationRecord
-  mount_uploader :image, ImageUploader
-  belongs_to :picture, optional: true
-end
--- app/models/picture.rb
+++
@@ -1,4 +0,0 @@
-class Picture < ApplicationRecord
-  mount_uploader :image, ImageUploader
-  has_one :converted_picture
-end
