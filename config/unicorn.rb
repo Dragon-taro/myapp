9c81eb787365950284f5697342b431d20e4c6c78
Dragon-taro <o_o_o_o_o1031@yahoo.co.jp>
2018-06-05 15:33:45 +0900

    実験

---
+++ config/unicorn.rb
@@ -0,0 +1,24 @@
+timeout 150
+worker_processes 1
+
+listen '/home/ec2-user/tmp/unicorn.sock'
+pid "tmp/pids/unicorn.pid"
+stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
+stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
+preload_app true
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+end
